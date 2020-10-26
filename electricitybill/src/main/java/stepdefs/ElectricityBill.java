e9f9c26697ca847e4cd06519c4537035d545f10e
sakshi.kapoor <sakshi.a.kapoor@accenture.com>
2020-10-26 17:31:57 +0530

    Electricity Kata on 26thOct - BDD skeleton complete

---
+++ electricitybill/src/main/java/stepdefs/ElectricityBill.java
@@ -0,0 +1,18 @@
+package stepdefs;
+
+public class ElectricityBill {
+	
+	
+
+	public void setUnits(Integer units) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	public int getUnits() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
